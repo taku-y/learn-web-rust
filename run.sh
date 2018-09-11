9189da8f840f656e1abe04b15eb642e1bd3f54d9
taku-y <taku.yoshioka.4096@gmail.com>
2018-09-11 18:21:40 +0900

    Export an additional port when running Docker container

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -it -p 8000:8000 -p 9001:9001 --rm \
+docker run -it -p 8000:8000 -p 9001:9001 -p 9002:9002 --rm \
     -v $(pwd)/workspace:/root/workspace \
     --name learn-web-rust learn-web-rust
